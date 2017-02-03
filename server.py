761136d9d54fd69995951cecfc65895b41c69f8e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-02 18:04:41 -0800

    Working log-in/log-out, added user info

--- server.py
+++ server.py
@@ -38,6 +38,28 @@ def user_list():
     return render_template("/user_list.html", users=users)
 
 
+@app.route('/users/<int:user_id>')
+def user_detail(user_id):
+    """show info about user"""
+
+    user = User.query.get(user_id)
+    return render_template("user.html", user=user)
+
+
+@app.route('/movies')
+def movie_list():
+    """Show list of movies"""
+
+    movies = Movie.query.order_by("title asc").all()
+    return render_template("/movie_list.html", movies=movies)
+
+# @app.route('/movies/<int:movie_id>')
+# def movie_detail(movie_id):
+#     """Show ratings for a movie"""
+
+#     movie = Movie.query.get(movie_id)
+#     return render_template("movie.html", movie=movie)
+
 @app.route('/register')
 def registration_form():
     """Prompts user for username and password"""
@@ -49,19 +71,31 @@ def log_in():
     """Verifies user within database and logs user in"""
 
     user = request.form.get("username")
-    password = request.form.get("password")
-
+    #password = request.form.get("password")
 
     try:
-#fix flash message!
-        User.query.filter_by(email = user).one()
-        messages = flash('You were successfully logged in')
+
+        current_user = User.query.filter_by(email=user).one()
+        flash('You were successfully logged in')
+        session["user_id"] = current_user.user_id
+
         return render_template('homepage.html')
 
     except sqlalchemy.orm.exc.NoResultFound:
 
         return render_template('/registration_form.html')
 
+
+@app.route('/log_out')
+def log_out():
+    """Removes a user from a session"""
+
+    del session["user_id"]
+    # print session["user_id"]
+    flash('You were successfully logged out')
+    return render_template('homepage.html')
+
+    #Additional reference for log in/log out can be found in project tracker project
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
